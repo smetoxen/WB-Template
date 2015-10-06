dd65d64e4d4515815d2e1ab5244c08e2a34f7ab2
Susan Metoxen <smetoxen@gmail.com>
2015-10-06 11:26:55 -0500

    Word Find Cleanup
    minor cleanup to game

--- word-find.php
+++ word-find.php
@@ -1,63 +1,45 @@
 <!DOCTYPE HTML>
+
 <html>
-	<head>
-	<?php require_once "header.php" ?>
-	
-	<!--The link to jQuery is included here rather than at the end of the body because the word find game requires the jQuery file to load at the beginning. The js-files.php file is not referenced on this page.-->
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<head>
+<?php require_once "header.php" ?>
+<script>
+if (screen.width <= 660) {
+document.location = "http://isthisanagate.com/wordfind_mobile.php";
+}
+</script>
+</head>
 
-
-	<!--This code redirects the webpage to a smaller word find game for devices with a screen width less than 660 pixels-->
-	<script>
-		if (screen.width <= 660) {
-			document.location = "http://isthisanagate.com/word-find-mobile.php";
-		}
-	</script>
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-	<style>
-<link rel="stylesheet" type="text/css" href="wordfind/jquery.wordsearchgame.css">
-</style>
-	</head>
-
-	<body>
+<body>
 <div class = "wrapper">
-
-	<?php require_once "nav.php" ?>
-
-		<div class="content wbcenter"> <img src="images/Minnesota.jpg" style="max-width:267px; width:30%; height:auto;float:left;margin:20px" alt="Minnesota State Sign">
-		<h1 style="margin-bottom:20px;text-align:left;color:#902828">The Great Minnesota Rock Hunt</h1>
-		<h3>What types of rocks can you find in Minnesota? There are many kinds of rocks in Minnesota, including the Lake Superior Agate, the official Minnesota gemstone! See if you can find them all. </h3>
-		<p>Photo by Andriy Kravchenko<br/>
-				Copyright: <a href='http://www.123rf.com/profile_andreykr'>andreykr / 123RF Stock Photo</a></p>
+	<div class="content "> <a href="index.php" target="_blank">HOME</a>
+		<h1 style="margin-bottom:20px;text-align:left;color:#902828">Word Find Game</h1>
+		<p>This word find game is based on the code by <a href="https://code.google.com/p/jquery-wordsearch-game/" target="_blank">Ryan Fernandes</a> and is covered by an MIT license. It has a lot of bells and whistles. As users find a word, there is an animation to cross it off the list of words. Note: The nav bar does not work on this game, as it is now. </p>
 		<div id="theGrid" style="display:block;clear:both"></div>
+		<div style="clear:both;display:block"> <br>
+			<hr/>
+			<br>
+		</div>
 	</div>
-		<!--End Content--> 
-		
-	</div>
+	<!--End Content--> 
+	
+</div>
+<?php require_once "footer.php" ?>
+<script type="text/javascript" src="wordfind/jquery-1.6.2.min.js"></script> 
 <script type="text/javascript" src="wordfind/jquery-ui-1.8.16.custom.min.js"></script> 
 <script type="text/javascript" src="wordfind/jquery.wordsearchgame.js"></script> 
-<script src="wordfind/jquery.ui.touch-punch.min.js"></script> 
+<script src="wordfind/jquery.ui.touch-punch.min.js"></script>
+<link  rel="stylesheet" type="text/css" href="wordfind/jquery.wordsearchgame.css">
 <script>
-		$(document).ready( function () {
-			var words = 			"lake,superior,agate,eye,banded,tube,amethyst,laker,keweenaw,moss,"+
-				"fossil,geode,chert,quartz,jasper,granite,sandstone,greenstone,taconite";
-	
-		//attach the game to a div
-		$("#theGrid").wordsearchwidget({"wordlist" : words,"gridsize" : 20});
+		$(document).ready( function () {var words = "javascript,html,css,php,programmer,developer,code,nerd,geek,loop,"+
+						"conditional,object,jquery,bootstrap,scope,github,abstract,hexadecimal,binary";
+			
+			//attach the game to a div
+			$("#theGrid").wordsearchwidget({"wordlist" : words,"gridsize" : 20});
 
 		});
 
-
-	</script>
-<?php require_once "footer.php"; ?>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
-<script>
-	 	$(document).ready(function(){
-			$(".dropdown-toggle").dropdown();
-		});
-     
-     
-     </script>
+    
+        </script>
 </body>
 </html>
