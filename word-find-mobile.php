de740d66759b67a6b8e35a98da0f8f5891e44b65
Susan Metoxen <smetoxen@gmail.com>
2015-10-06 13:04:37 -0500

    Word-find Game
    More clean up on the Word-Find game.

--- word-find-mobile.php
+++ word-find-mobile.php
@@ -6,40 +6,26 @@
 </head>
 
 <body>
-<div class = "wrapper" style="width:100%"> <img src="images/webbanner.png" style="max-width:1200px;width:100%;height:auto;margin-left:auto;margin-right:auto;margin-top:-50px;margin-bottom:0px;display:block;box-shadow:none" />
-	<div class="content" style="margin-left:auto;margin-right:auto;display:block;width:100%"> <a href="index.php" style="font-size:1.3em">Home</a> | <a href="mailto:?subject=Try this fun Word Find about Agates&amp;body=Check out this word find that is all about Lake Superior Agates! http://www.IsThisAnAgate.com/wordfind.php." title="Share by Email" style="font-size:1.3em">Share with a Friend</a> <img src="images/Minnesota.jpg" style="max-width:267px; width:30%; height:auto;float:left;margin:20px">
-		<h3 style="margin-bottom:20px;text-align:left;color:#902828">The Great Minnesota Rock Hunt</h3>
-		<p>What types of rocks can you find in Minnesota? There are many kinds of rocks in Minnesota, including the Lake Superior Agate! See if you can find them all. </p>
-		<p>Photo by Andriy Kravchenko<br/>
-			Copyright: <a href='http://www.123rf.com/profile_andreykr'>andreykr / 123RF Stock Photo</a></p>
+<div class = "wrapper" style="width:100%;margin-top:-50px"> <img src="images/header-small.png"  class="banner-img" />
+	<div class="content" style="margin-left:auto;margin-right:auto;display:block;width:100%"> <a href="index.php" style="font-size:1.3em">Home</a>
+		<h3 style="margin-bottom:20px;text-align:left;color:#902828">Word Find Mobile</h3>
+		<p>This word find game is based on the code by <a href="https://code.google.com/p/jquery-wordsearch-game/" target="_blank">Ryan Fernandes</a> and is covered by an MIT license.</p>
 		<div id="theGrid" style="display:block;margin:0px;padding:0px"></div>
-		<div style="background-color:white;clear:both"> <br>
-			<br>
-			<hr/>
-			<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script> 
-			<!-- WordFind --> 
-			<ins class="adsbygoogle"
-     style="display:block"
-     data-ad-client="ca-pub-4660841321215547"
-     data-ad-slot="9343635510"
-     data-ad-format="auto"></ins> 
-			<script>
-(adsbygoogle = window.adsbygoogle || []).push({});
-</script> 
-		</div>
+		<br>
+		<hr/>
 	</div>
-	<!--End Content--> 
-	
 </div>
-<?php require_once "footer.php" ?>
+<!--End Content-->
+
+<?php require_once "footer.php"; ?>
+</div>
 <script type="text/javascript" src="wordfind/jquery-1.6.2.min.js"></script> 
 <script type="text/javascript" src="wordfind/jquery-ui-1.8.16.custom.min.js"></script> 
 <script type="text/javascript" src="wordfind/jquery.wordsearchgame.js"></script> 
 <script src="wordfind/jquery.ui.touch-punch.min.js"></script>
 <link  rel="stylesheet" type="text/css" href="wordfind/jquery.wordsearchgame.css">
 <script>
-		$(document).ready( function () {var words = "agate,eye,tube,laker,moss,"+
-						"fossil,geode,quartz,jasper,granite";
+		$(document).ready( function () {var words = "html,css,php,code,nerd,geek,loop";
 			
 			//attach the game to a div
 			$("#theGrid").wordsearchwidget({"wordlist" : words,"gridsize" : 8});
@@ -49,6 +35,5 @@
     
         </script>
 <?php require_once "footer.php"; ?>
-<?php require_once "js-files.php"; ?>
 </body>
 </html>
--- word-find.php
+++ word-find.php
@@ -5,14 +5,17 @@
 <?php require_once "header.php" ?>
 <script>
 if (screen.width <= 660) {
-document.location = "http://isthisanagate.com/wordfind_mobile.php";
+document.location = "http://whitebuffalosolutions.com/WB-Template/word-find-mobile.php";
 }
 </script>
 </head>
 
 <body>
-<div class = "wrapper">
-	<div class="content "> <a href="index.php" target="_blank">HOME</a>
+<!--The Bootstrap nav bar conflicts with the game javascript. As a quick fix I removed the nav bar and added a home button --> 
+
+<!--Added -50px to the top margin to compensate for the missing nav bar -->
+<div class = "wrapper" style="margin-top:-50px"> <img src="images/header-small.png"  class="banner-img" />
+	<div class="content"> <a href="index.php" target="_blank">HOME</a>
 		<h1 style="margin-bottom:20px;text-align:left;color:#902828">Word Find Game</h1>
 		<p>This word find game is based on the code by <a href="https://code.google.com/p/jquery-wordsearch-game/" target="_blank">Ryan Fernandes</a> and is covered by an MIT license. It has a lot of bells and whistles. As users find a word, there is an animation to cross it off the list of words. Note: The nav bar does not work on this game, as it is now. </p>
 		<div id="theGrid" style="display:block;clear:both"></div>
@@ -21,16 +24,15 @@ document.location = "http://isthisanagate.com/wordfind_mobile.php";
 			<br>
 		</div>
 	</div>
-	<!--End Content--> 
+	<!--End Content-->
 	
-</div>
-<?php require_once "footer.php" ?>
-<script type="text/javascript" src="wordfind/jquery-1.6.2.min.js"></script> 
-<script type="text/javascript" src="wordfind/jquery-ui-1.8.16.custom.min.js"></script> 
-<script type="text/javascript" src="wordfind/jquery.wordsearchgame.js"></script> 
-<script src="wordfind/jquery.ui.touch-punch.min.js"></script>
-<link  rel="stylesheet" type="text/css" href="wordfind/jquery.wordsearchgame.css">
-<script>
+	<?php require_once "footer.php" ?>
+	<script type="text/javascript" src="wordfind/jquery-1.6.2.min.js"></script> 
+	<script type="text/javascript" src="wordfind/jquery-ui-1.8.16.custom.min.js"></script> 
+	<script type="text/javascript" src="wordfind/jquery.wordsearchgame.js"></script> 
+	<script src="wordfind/jquery.ui.touch-punch.min.js"></script>
+	<link  rel="stylesheet" type="text/css" href="wordfind/jquery.wordsearchgame.css">
+	<script>
 		$(document).ready( function () {var words = "javascript,html,css,php,programmer,developer,code,nerd,geek,loop,"+
 						"conditional,object,jquery,bootstrap,scope,github,abstract,hexadecimal,binary";
 			
@@ -40,6 +42,7 @@ document.location = "http://isthisanagate.com/wordfind_mobile.php";
 		});
 
     
-        </script>
+        </script> 
+</div>
 </body>
 </html>
